ca4c94e2b4687ac4877c93cc0f34886a65be644d
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-01-05 00:34:28 +0900

    :sparkles: Implement basic of d3

--- 02/2.04/js/main.js
+++ 02/2.04/js/main.js
@@ -1,15 +1,28 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.4 - Adding SVGs with D3
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    2.4 - Adding SVGs with D3
+ */
 
-const svg = d3.select("#chart-area").append("svg")
-  .attr("width", 400)
-  .attr("height", 400)
+// const data = [25, 20, 10, 12, 15];
 
-svg.append("circle")
-  .attr("cx", 100)
-  .attr("cy", 250)
-  .attr("r", 70)
-  .attr("fill", "red")
+d3.csv("./js/ages.csv").then((data) => {
+  data.forEach((d) => {
+    d.age = Number(d.age);
+  });
+  console.log(data);
+  const svg = d3
+    .select("#chart-area")
+    .append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+
+  const circles = svg.selectAll("circle").data(data);
+
+  circles
+    .enter()
+    .append("circle")
+    .attr("cx", (d, i) => i * 60)
+    .attr("cy", 250)
+    .attr("r", (d) => d.age);
+});
--- 02/2.08.0/js/main.js
+++ 02/2.08.0/js/main.js
@@ -1,5 +1,32 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.8 - Activity: Your first visualization!
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    2.8 - Activity: Your first visualization!
+ */
+
+d3.json("data/buildings.json").then((data) => {
+  // Parsing the number
+  data.forEach((d) => {
+    d.height = Number(d.height);
+  });
+
+  // creating svg
+  const svg = d3
+    .select("#chart-area")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 500);
+
+  // data join
+  const rects = svg.selectAll("rect").data(data);
+
+  // define each rects
+  rects
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => 40 * i)
+    .attr("y", 10)
+    .attr("width", 30)
+    .attr("height", (d) => d.height)
+    .attr("fill", "gray");
+});
