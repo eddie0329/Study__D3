4d74885e9093e17fecaace4a38976293da5287d1
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-01-13 00:27:12 +0900

    :sparkles: Implement update pattern

--- 05/5.02/js/main.js
+++ 05/5.02/js/main.js
@@ -1,19 +1,24 @@
 /*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    5.2 - Looping with intervals
-*/
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    5.2 - Looping with intervals
+ */
 
-const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
-const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 };
+const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM;
 
-const svg = d3.select("#chart-area").append("svg")
+let flag = true;
+
+const svg = d3
+  .select("#chart-area")
+  .append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
-  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
 
-const g = svg.append("g")
-  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
 // X label
 g.append("text")
@@ -22,62 +27,105 @@ g.append("text")
   .attr("y", HEIGHT + 60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
-  .text("Month")
+  .text("Month");
 
 // Y label
-g.append("text")
+const yLabel = g
+  .append("text")
   .attr("class", "y axis-label")
-  .attr("x", - (HEIGHT / 2))
+  .attr("x", -(HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Revenue ($)")
+  .text("Revenue ($)");
+const x = d3.scaleBand().range([0, WIDTH]).paddingInner(0.3).paddingOuter(0.2);
 
-d3.csv("data/revenues.csv").then(data => {
-  data.forEach(d => {
-    d.revenue = Number(d.revenue)
-  })
+const y = d3.scaleLinear().range([HEIGHT, 0]);
 
-  const x = d3.scaleBand()
-    .domain(data.map(d => d.month))
-    .range([0, WIDTH])
-    .paddingInner(0.3)
-    .paddingOuter(0.2)
-  
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.revenue)])
-    .range([HEIGHT, 0])
+const xAxisGroup = g
+  .append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, ${HEIGHT})`);
 
-  const xAxisCall = d3.axisBottom(x)
-  g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0, ${HEIGHT})`)
-    .call(xAxisCall)
-    .selectAll("text")
-      .attr("y", "10")
-      .attr("x", "-5")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-40)")
+const yAxisGroup = g.append("g").attr("class", "y axis");
 
-  const yAxisCall = d3.axisLeft(y)
-    .ticks(3)
-    .tickFormat(d => d + "m")
-  g.append("g")
-    .attr("class", "y axis")
-    .call(yAxisCall)
-
-  const rects = g.selectAll("rect")
-    .data(data)
-  
-  rects.enter().append("rect")
-    .attr("y", d => y(d.revenue))
-    .attr("x", (d) => x(d.month))
-    .attr("width", x.bandwidth)
-    .attr("height", d => HEIGHT - y(d.revenue))
-    .attr("fill", "grey")
+d3.csv("data/revenues.csv").then((data) => {
+  data.forEach((d) => {
+    d.revenue = Number(d.revenue);
+    d.profit = Number(d.profit);
+  });
 
   d3.interval(() => {
-    console.log("Hello World")
-  }, 1000)
-})
+    flag = !flag;
+    const newData = flag ? data : data.slice(1);
+    update(newData);
+  }, 1000);
+  update(data);
+});
+
+function update(data) {
+  const value = flag ? "profit" : "revenue";
+
+  const t = d3.transition().duration(700);
+
+  x.domain(data.map((d) => d.month));
+  y.domain([0, d3.max(data, (d) => d[value])]);
+
+  const xAxisCall = d3.axisBottom(x);
+
+  xAxisGroup
+    .transition(t)
+    .call(xAxisCall)
+    .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)");
+
+  const yAxisCall = d3
+    .axisLeft(y)
+    .ticks(3)
+    .tickFormat((d) => d + "m");
+
+  yAxisGroup.transition(t).call(yAxisCall);
+
+  // DATA JOIN
+  const rects = g.selectAll("rect").data(data, (d) => d.month);
+
+  console.log(rects);
+
+  // EXIT
+  rects
+    .exit()
+    .attr("fill", "red")
+    .transition(t)
+    .attr("height", 0)
+    .attr("y", y(0))
+    .remove();
+
+  // UPDATE
+  // rects
+  //   .transition(t)
+  //   .attr("y", (d) => y(d[value]))
+  //   .attr("x", (d) => x(d.month))
+  //   .attr("width", x.bandwidth)
+  //   .attr("height", (d) => HEIGHT - y(d[value]));
+
+  yLabel.text(flag ? "Profit ($)" : "Revenue ($)");
+
+  // ENTER
+  rects
+    .enter()
+    .append("rect")
+    .attr("fill", "grey")
+    .attr("y", y(0))
+    .attr("height", 0)
+    // UPDATE
+    .merge(rects)
+    .transition(t)
+    .attr("x", (d) => x(d.month))
+    .attr("y", (d) => y(d[value]))
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => HEIGHT - y(d[value]));
+}
